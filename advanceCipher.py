3e07cb04682f75ab4360cacdaad2350fca849212
HarryStemwork <162728191+HarryStemwork@users.noreply.github.com>
2024-07-16 10:28:25 +0800

    Create advanceCipher.py

---
+++ advanceCipher.py
@@ -0,0 +1,16 @@
+abc = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+front3 = #how to take the first 3 in a list
+end23 = #how to take the last 23 in a list
+subText = end23 + front3
+encryptDict = #how to zip two list into a dict
+
+word = #how to let user input
+cipher = []
+for char in word:
+    newChar = #how to locate the new character in dict
+    cipher.#how to add newChar to the list-cipher
+
+cipherText = "".join(cipher)
+
+print(f"Origial Word: {word}")
+print(f"Encrypted Word: {cipherText}")
